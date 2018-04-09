27a6a62206be9110bed11b9e6f7308b2575c7526
David Lam <david.lam@winc.com.au>
2018-04-09 16:14:00 +1000

    added Authenticator2 and authenticate2 to bypass impersonation.

--- handler.go
+++ handler.go
@@ -1,13 +1,21 @@
 package authenticator
 
 import (
+	"bytes"
 	"encoding/base64"
-	"fmt"
+	"encoding/binary"
+	//	"math"
+	//	"strconv"
+	//	"fmt"
 	"log"
 	"net/http"
+	//	"strconv"
+
 	"os/user"
 	"runtime"
 	"strings"
+
+	ctx "context"
 
 	"github.com/alexbrainman/sspi"
 	"github.com/alexbrainman/sspi/ntlm"
@@ -29,29 +37,55 @@ func init() {
 
 func initiateNTLM(w http.ResponseWriter) {
 	w.Header().Set("WWW-Authenticate", "NTLM")
+	//	w.Header().Set("Connection", "Keep-Alive")
+	//	w.Header().Set("Content-type", "text/xml")
 	http.Error(w, "Authorization required", http.StatusUnauthorized)
 	return
 }
 
 func authenticate(c *ntlm.ServerContext, authenticate []byte) (u *user.User, err error) {
+
 	defer c.Release()
 	err = c.Update(authenticate)
+
 	if err != nil {
+
 		return
 	}
+
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
+
 	err = c.ImpersonateUser()
 	if err != nil {
 		return
 	}
+
 	defer c.RevertToSelf()
+
 	u, err = user.Current()
+	runtime.UnlockOSThread()
+
+	return
+}
+
+func authenticate2(c *ntlm.ServerContext, authenticate []byte) (u *user.User, err error) {
+
+	defer c.Release()
+	err = c.Update(authenticate)
+
+	if err != nil {
+
+		return
+	}
+
+	u, err = user.Current()
+
 	return
 }
 
 func authorize(u *user.User, r *http.Request) bool {
-	fmt.Println(u.Uid+" ("+u.Username+") wants ", r.URL.String())
+	//	fmt.Println(u.Uid+" ("+u.Username+") wants ", r.URL.String())
 	return true
 }
 
@@ -102,6 +136,64 @@ func Authenticator(next http.Handler) http.Handler {
 		}
 		w.Write([]byte("You are authenticated as " + u.Username + "\r\n"))
 		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
+func Authenticator2(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		if false && len(r.RequestURI) >= 3 && r.RequestURI[0:3] != "/g/" {
+			next.ServeHTTP(w, r)
+			return
+		}
+		var err error
+		auth := r.Header.Get("Authorization")
+		if auth == "" || (len(strings.SplitN(auth, " ", 2)) < 2) {
+			initiateNTLM(w)
+			return
+		}
+		parts := strings.SplitN(auth, " ", 2)
+		authType := parts[0]
+		if authType != "NTLM" {
+			initiateNTLM(w)
+			return
+		}
+		var authPayload []byte
+		authPayload, err = base64.StdEncoding.DecodeString(parts[1])
+		context, ok := contexts[r.RemoteAddr]
+		if !ok {
+			sendChallenge(authPayload, w, r)
+			return
+		}
+		defer delete(contexts, r.RemoteAddr)
+		var u *user.User
+		u, err = authenticate2(context, authPayload)
+		if err != nil {
+			log.Println("auth error:", u, err)
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		userName := ""
+		if authPayload[8] == 3 {
+
+			var length int16
+			binary.Read(bytes.NewReader([]byte{authPayload[36], authPayload[37]}), binary.LittleEndian, &length)
+			//			log.Println(length)
+			var offset int16
+			binary.Read(bytes.NewReader([]byte{authPayload[40], authPayload[41]}), binary.LittleEndian, &offset)
+			//			log.Println(offset)
+			var buf []byte
+			for i, v := range authPayload[offset : offset+length] {
+				if i == ((i / 2) * 2) {
+					buf = append(buf, v)
+				}
+			}
+
+			userName = string(buf)
+		}
+
+		c := ctx.WithValue(r.Context(), "sso", userName)
+		next.ServeHTTP(w, r.WithContext(c))
 	}
 	return http.HandlerFunc(fn)
 }
